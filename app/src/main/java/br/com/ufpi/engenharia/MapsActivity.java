365b4dca43954bb88f644457cd684d142c27afe6
moisesbispo <moisesbbispo@gmail.com>
2017-01-18 13:10:20 -0300

    Update MapsActivity.java
    -Adicionado as funcionalidades de mostrar todos os imóveis no mapa, feito a partir das informações de latitude e longitude baseado no endereço

--- app/src/main/java/br/com/ufpi/engenharia/MapsActivity.java
+++ app/src/main/java/br/com/ufpi/engenharia/MapsActivity.java
@@ -1,7 +1,8 @@
 package br.com.ufpi.engenharia;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,6 +10,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+import br.com.ufpi.engenharia.entidade.Imovel;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -24,17 +29,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+
     /**
-     * Show the map for the current location.
-     * @param googleMap
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        Intent i = getIntent();
+        List<Imovel> imoveis = null;
 
+        imoveis = (List<Imovel>) i.getSerializableExtra("LIST");
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        for(Imovel im : imoveis){
+            LatLng ll = new LatLng(im.getLatA(), im.getLongA());
+            mMap.addMarker(new MarkerOptions()
+                    .position(ll)
+                    .title(im.getNome()));
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(ll));
+        }
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+
     }
 }
