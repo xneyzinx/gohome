ddcc61b25bcd5e393eb860699425b7311d669402
moisesbispo <moisesbbispo@gmail.com>
2017-01-18 13:06:37 -0300

    Update MainActivity.java
    -Atualizado o botão Mostrar no Mapa, função de mostrar no mapa todos os imóveis cadastrados foi adicionada.

--- app/src/main/java/br/com/ufpi/engenharia/MainActivity.java
+++ app/src/main/java/br/com/ufpi/engenharia/MainActivity.java
@@ -1,20 +1,20 @@
 package br.com.ufpi.engenharia;
 
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.support.annotation.NonNull;
-        import android.support.v7.app.AppCompatActivity;
-        import android.support.v7.widget.Toolbar;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.ProgressBar;
-        import android.widget.Toast;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-        import com.google.android.gms.tasks.OnCompleteListener;
-        import com.google.android.gms.tasks.Task;
-        import com.google.firebase.auth.FirebaseAuth;
-        import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+        import java.io.Serializable;
+        import java.util.List;
+
+        import br.com.ufpi.engenharia.controle.ControleImovel;
+        import br.com.ufpi.engenharia.entidade.Imovel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener authListener;
     private FirebaseAuth auth;
     private Button cadastrarImovel;
+    private List<Imovel> imoveis = null;
+    private ControleImovel controleImovel = new ControleImovel(MainActivity.this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +67,10 @@ public class MainActivity extends AppCompatActivity {
         mostrarNoMapa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MapsLocationActivity.class));
+                Intent i = new Intent(MainActivity.this, MapsActivity.class);
+                imoveis = controleImovel.buscarImovel();
+                i.putExtra("LIST", (Serializable) imoveis);
+                startActivity(i);
             }
         });
     }
